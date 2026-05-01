1106d08b5ff631fce5e03904245be1bb1ee42084
Yash Sagariye <sagariyeyash@gmail.com>
2026-05-02 03:24:01 +0530

    fix domain allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: ['vajraa-frontend.onrender.com'],
-    proxy: {
-      '/api': {
-        target: 'https://vajraa-backend.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
   preview: {
     host: true,
-    allowedHosts: ['vajraa-frontend.onrender.com'],
+    allowedHosts: ['vajraflow.net', 'www.vajraflow.net']
   },
+  server: {
+    host: true
+  }
 });
