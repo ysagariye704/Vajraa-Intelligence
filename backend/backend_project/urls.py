2eb25ac3af16fc8e269253643a0418af819483fc
Yash Sagariye <sagariyeyash@gmail.com>
2026-05-19 14:53:27 +0530

    fix django root endpoint

--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -1,10 +1,16 @@
-from django.http import HttpResponse
+from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
 
 def home(request):
-    return HttpResponse("Backend is running 🚀")
+    return JsonResponse({
+        "status": "Backend running successfully"
+    })
+
 
 urlpatterns = [
-    path('', home),
-    path('api/', include('api.urls')),
+    path("", home),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
 ]
