249eecb57a167cb1fb4208b871f88a1171fa9964
Yash Sagariye <sagariyeyash@gmail.com>
2026-05-19 16:02:19 +0530

    fix render frontend deployment

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: "automatic"
-    })
-  ],
-  server: {
-    host: true
-  },
-  preview: {
-    host: true,
-    allowedHosts: ["vajraflow.net", "www.vajraflow.net"]
-  }
+  plugins: [react()],
 });
